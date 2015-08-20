eb2e1faee2cb4d2921bb2f7ba1afe27820f065f8
mfahiz <mfahiz@zaizi.com>
2015-08-20 13:48:17 +0530

    Maven command line error fixed

--- sensefy-authserver/src/main/java/com/zaizi/user/acl/ManifoldACLRequester.java
+++ sensefy-authserver/src/main/java/com/zaizi/user/acl/ManifoldACLRequester.java
@@ -27,6 +27,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.zaizi.exception.SensefyException;
+import com.zaizi.user.acl.ACLRequester;
 
 /**
  * ACL Requester. Make requests to ManifoldCF Authority Service
