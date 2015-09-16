14387e140c9411d30096a2cfa3e77c3a65635e53
mfahiz <mfahiz@zaizi.com>
2015-09-16 10:00:49 +0530

    Moved URI to a common class

---
+++ sensefy-api/src/main/java/com/zaizi/sensefy/api/configuration/model/RequestURI.java
@@ -0,0 +1,25 @@
+/**
+ * @author Fahiz Mohamed
+ * @since  
+ */
+package com.zaizi.sensefy.api.configuration.model;
+
+/**
+ * An abstraction for a Sensefy API URI
+ * @author mfahiz
+ *
+ */
+public class RequestURI {
+	
+	/**
+	 * Keyword Search Path
+	 */
+	 public final static String KEYWORD_SEARCH = "/keywordSearch";
+	 public final static String AUTOCOMPLETE_1 = "/autocomplete/1";
+	 public final static String AUTOCOMPLETE_2 = "/autocomplete/2";
+	 public final static String AUTOCOMPLETE_3 = "/autocomplete/3";
+	 public final static String ENTITY_DRIVEN_SEARCH = "/entityDrivenSearch";
+	 public final static String SHOW_ENTITY_BY_DOCID = "/showEntitiesByDocId";
+}
+
+
