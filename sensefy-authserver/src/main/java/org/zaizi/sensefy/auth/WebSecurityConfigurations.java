d38d1b3c2a5d692abbf11a11ac1d02d5d741e05e
Chalitha Perera <cperera@zaizi.com>
2015-12-15 15:01:11 +0530

    simple fix for SENSEFY-1395

--- sensefy-authserver/src/main/java/org/zaizi/sensefy/auth/WebSecurityConfigurations.java
+++ sensefy-authserver/src/main/java/org/zaizi/sensefy/auth/WebSecurityConfigurations.java
@@ -24,12 +24,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
@@ -48,13 +51,16 @@ class LoginConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private AuthenticationManager authenticationManager;
+	
+	@Value("${sensefy.search.endpoint}")
+	private String searchEndpoint;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-
-		http.formLogin().loginPage("/login").permitAll().and().requestMatchers()
+		
+		http.formLogin().loginPage("/login").permitAll().defaultSuccessUrl(searchEndpoint).and().requestMatchers()
 				.antMatchers("/login", "/oauth/authorize", "/oauth/confirm_access").and().authorizeRequests()
-				.anyRequest().authenticated().and().csrf().csrfTokenRepository(csrfTokenRepository()).and()
+				.anyRequest().authenticated().and().logout().permitAll().and().csrf().csrfTokenRepository(csrfTokenRepository()).and()
 				.addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
 
 	}
