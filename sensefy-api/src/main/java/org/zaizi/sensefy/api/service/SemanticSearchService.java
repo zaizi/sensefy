7ef4985bcfb8fa5517271c3eb56956abb918cbe2
ChalithaUdara <cperera@zaizi.com>
2016-06-14 14:18:15 +0530

    added entity id for search result

--- sensefy-api/src/main/java/org/zaizi/sensefy/api/service/SemanticSearchService.java
+++ sensefy-api/src/main/java/org/zaizi/sensefy/api/service/SemanticSearchService.java
@@ -275,7 +275,7 @@ public class SemanticSearchService extends SolrService {
 			SolrQuery entityQuery = new SolrQuery();
 			entityQuery.setRequestHandler("/get");
 			entityQuery.set("ids", entityId);
-			entityQuery.setFields("description","thumbnail","label");
+			entityQuery.setFields("description","thumbnail","label", "id");
 			QueryResponse entityResponse;
 			entityResponse = this.getEntityCore().query(entityQuery);
 			List<SolrDocument> entities = entityResponse.getResults();
