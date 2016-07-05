2c0c52e237e44861c7687374298b6ec19d62e6f4
ChalithaUdara <cperera@zaizi.com>
2016-07-05 10:16:15 +0530

    increase number of entity suggestions

--- sensefy-api/src/main/java/org/zaizi/sensefy/api/service/SolrSmartAutoCompleteService.java
+++ sensefy-api/src/main/java/org/zaizi/sensefy/api/service/SolrSmartAutoCompleteService.java
@@ -129,7 +129,7 @@ public class SolrSmartAutoCompleteService extends SolrService {
 			if (semantic) {
 				// First query to suggest named entities, showing mini details
 				// <field>:<value>
-				numberOfSuggestions = 6;
+				numberOfSuggestions = 8;
 				QueryResponse namedEntitiesQueryResponse = getSmartSuggestions(termToComplete, numberOfSuggestions,
 						this.getEntityCore());
 
